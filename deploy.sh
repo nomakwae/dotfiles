dd916661808745d7dedfdce583de55b6040da853
nomakwae <30879600+nomakwae@users.noreply.github.com>
2026-01-17 00:30:07 -0500

    deploy.sh: make it more visually appealing

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+DEPLOY="\e[01;31m[DEPLOY]:\e[00m"
+
 if command -v sudo &>/dev/null; then
 	DOAS="sudo"
 elif command -v doas &>/dev/null; then
@@ -8,17 +10,17 @@ fi
 
 # checking for lsp support
 if ! command -v typescript-language-server &>/dev/null; then
-	echo "installing typescript-language-server"
+	echo -e "${DEPLOY} installing typescript-language-server"
 	"$DOAS" npm install -g typescript typescript-language-server
 fi
 
 if ! command -v gopls &>/dev/null; then
-	echo "installing gopls"
+	echo -e "${DEPLOY} installing gopls"
 	go install golang.org/x/tools/gopls@latest
 fi
 
 if ! command -v emmet-language-server &>/dev/null; then
-	echo "installing emmet-language-server"
+	echo -e "${DEPLOY} installing emmet-language-server"
 	"$DOAS" npm i -g @olrtg/emmet-language-server
 fi
 prompt() {
@@ -33,11 +35,11 @@ prompt() {
 			[Yy]* )
 				rm -r $1
 				cp -r $2 $3
-				echo "overwrote $1"
-				break ;;
+				echo -e "${DEPLOY} overwrote $1"
+				return 0 ;;
 			[Nn]* )
-				echo "skipping..."
-				break ;;
+				echo -e "${DEPLOY} skipping..."
+				return 1 ;;
 			[Dd]* )
 				diff --color=auto -r $1 $2
 				;;
@@ -51,29 +53,36 @@ CONFIG_HOME="${HOME}/.config"
 
 NVIM_DIR="${CONFIG_HOME}/nvim"
 if [[ -d "${NVIM_DIR}" ]]; then
-	echo "${NVIM_DIR} already exists."
+	echo -e "${DEPLOY} ${NVIM_DIR} already exists."
 	if ! diff -r "${NVIM_DIR}" nvim/ &>/dev/null; then
 		prompt "${NVIM_DIR}" "nvim/" "${CONFIG_HOME}"
 	else
-		echo "files are the same, skipping."
+		echo -e "${DEPLOY} files are the same, skipping."
 		:
 	fi
 else
 	cp -r nvim/ "${CONFIG_HOME}"
 fi
 
-ST_DIR = "${CONFIG_HOME}/st"
+ST_DIR="${CONFIG_HOME}/st"
 if command -v st &>/dev/null && [[ -d "${ST_DIR}" ]]; then
-	if ! diff st/x.c "${ST_DIR}/x.c" &>/dev/null; then
-		prompt "${ST_DIR}" "st/" "${CONFIG_HOME}"
-		cd "${ST_DIR}"
-		"${DOAS}" make clean install
+	echo -e "${DEPLOY} ${ST_DIR} already exists."
+	if ! diff -r "${ST_DIR}" st &>/dev/null; then
+		if prompt "${ST_DIR}" "st/" "${CONFIG_HOME}"; then
+			cd "${ST_DIR}"
+			echo -e "${DEPLOY} compiling st"
+			"${DOAS}" make clean install
+			cd -
+		fi
 	else
-		echo "files are the same, skipping."
+		echo -e "${DEPLOY} files are the same, skipping."
 		:
 	fi
 else
 	cp -r st/ "${CONFIG_HOME}"
 	cd "${ST_DIR}"
+	echo -e "${DEPLOY} compiling st"
 	"${DOAS}" make install
+	cd -
 fi
+
