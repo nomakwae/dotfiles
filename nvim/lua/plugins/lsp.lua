445ef171aa8e96260e44908d3042f0fcc769dcc4
nomakwae <30879600+nomakwae@users.noreply.github.com>
2026-02-10 23:28:47 -0500

    change warning levels

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -80,8 +80,8 @@ return {
 				format = function(diagnostic)
 					local diagnostic_message = {
 						[vim.diagnostic.severity.ERROR] = diagnostic.message,
-						[vim.diagnostic.severity.WARN] = diagnostic.message,
-						[vim.diagnostic.severity.INFO] = diagnostic.message,
+						-- [vim.diagnostic.severity.WARN] = diagnostic.message,
+						-- [vim.diagnostic.severity.INFO] = diagnostic.message,
 						-- [vim.diagnostic.severity.HINT] = diagnostic.message,
 					}
 					return diagnostic_message[diagnostic.severity]
